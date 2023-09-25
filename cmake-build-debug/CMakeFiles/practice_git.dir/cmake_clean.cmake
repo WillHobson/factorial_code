70dc4bcccea05b744d78c6d0a57d72bf8fe913f5
WillHobson <ni20088@bristol.ac.uk>
2023-09-25 11:26:15 +0100

    cmake debugger

---
+++ cmake-build-debug/CMakeFiles/practice_git.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practice_git.dir/main.cpp.o"
+  "CMakeFiles/practice_git.dir/main.cpp.o.d"
+  "practice_git"
+  "practice_git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practice_git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
